eca465671203931f54293d5c8ad1df65739766ea
Gonzalo De Varona <gonzalodevarona@Gonzalos-MacBook-Air.local>
2025-07-26 03:01:24 -0500

    feat: finished hex java users crud

--- src/main/java/com/gonza/pna/infrastructure/persistance/UserEntity.java
+++ src/main/java/com/gonza/pna/infrastructure/persistance/UserEntity.java
@@ -1,21 +1,67 @@
 package com.gonza.pna.infrastructure.persistance;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Table(name = "users")
 public class UserEntity {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private int age;
+
+
+    public UserEntity() {
+    }
+
+
+    public UserEntity(Long id, String name, String email, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
